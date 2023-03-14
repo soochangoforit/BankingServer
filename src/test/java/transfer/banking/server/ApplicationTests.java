611168a8c1f4b556957c9aed36abf032078d4740
soochan <tncksdl05@gmail.com>
2023-03-14 18:08:52 +0900

    initial commit

---
+++ src/test/java/transfer/banking/server/ApplicationTests.java
@@ -0,0 +1,13 @@
+package transfer.banking.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
