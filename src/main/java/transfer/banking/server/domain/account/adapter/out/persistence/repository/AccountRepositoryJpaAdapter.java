3e1ca996272358769e816092d92f3043ca1619c3
soochan <tncksdl05@gmail.com>
2023-03-30 13:02:44 +0900

    refactor : spring transactional annotation을 service 계층으로 올림(account) (#39)

--- src/main/java/transfer/banking/server/domain/account/adapter/out/persistence/repository/AccountRepositoryJpaAdapter.java
+++ src/main/java/transfer/banking/server/domain/account/adapter/out/persistence/repository/AccountRepositoryJpaAdapter.java
@@ -4,7 +4,6 @@ import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 import transfer.banking.server.domain.account.adapter.out.persistence.entity.Bank;
 import transfer.banking.server.domain.account.application.port.out.AccountRepositoryPort;
 import transfer.banking.server.domain.account.adapter.out.persistence.entity.Account;
@@ -23,14 +22,12 @@ public class AccountRepositoryJpaAdapter implements AccountRepositoryPort {
   private final AccountMapper accountMapper;
 
   @Override
-  @Transactional(readOnly = true)
   public boolean existsByAccountNumber(String accountNumber) {
     log.info("계좌번호가 존재하는지 확인합니다. accountNumber: {}", accountNumber);
     return accountRepository.existsByAccountNumber(accountNumber);
   }
 
   @Override
-  @Transactional
   public AccountDomain save(AccountDomain accountDomain) {
     Account account = accountMapper.toJpaEntity(accountDomain);
     log.info("계좌를 저장합니다. accountNumber: {}", account.getAccountNumber());
@@ -39,7 +36,6 @@ public class AccountRepositoryJpaAdapter implements AccountRepositoryPort {
   }
 
   @Override
-  @Transactional(readOnly = true)
   public Optional<AccountDomain> findAccountByBankAndNumber(Bank accountBank, String accountNumber) {
     log.info("계좌번호로 계좌 아이디를 조회합니다. accountBank: {}, accountNumber: {}", accountBank, accountNumber);
     return accountRepository.findAccountIdByBankAndNumber(accountBank, accountNumber)
@@ -47,7 +43,6 @@ public class AccountRepositoryJpaAdapter implements AccountRepositoryPort {
   }
 
   @Override
-  @Transactional
   public Optional<AccountDomain> findAccountByBankAndNumberWithLock(Bank bank,
       String accountNumber) {
     log.info("계좌번호로 계좌 아이디를 조회합니다. (락) accountBank: {}, accountNumber: {}", bank, accountNumber);
